df2911e22fd8f98c028acb0179b2882412b93377
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-01-19 15:15:21 -0600

    added list action on second page/ next step, dig through for info needed

--- app/app.js
+++ app/app.js
@@ -7,12 +7,47 @@ angular.module('MushroomMania', ['ngRoute'])
                 controller: 'RootCtrl',
                 templateUrl: '/partials/mushroom.html'
             })
+            .when('/mushroom/:name', {
+                controller:'MushroomCtrl',
+                templateUrl: '/partials/list.html'
+        })
     })
 
 .controller('RootCtrl', function($scope) {
     console.log('I am a RootCtrl')
     $scope.gotoMushroom = () => {
 
-        $scope.gotoMushroom = () => $location.url(`/mushroom/${$scope.name}`)
+        $scope.gotoMushroom = () => $location.url(`/mushroom/${$scope.mushroom}`)
+    }
+})
+
+.controller('MushroomCtrl', function($scope, $routeParams, mushroomFactory) {
+    console.log('I am a MushroomCtrl')
+
+    mushroomFactory.getMushroom($routeParams.mushroom)
+        .then((mushroom) => {
+            $scope.name = mushroom.name
+            $scope.edible = mushroom.edible
+        })
+    })
+
+// .factory('mushroomFactory', ($http) => {
+//     return {
+//         getMushroom (name) {
+//             return $http
+//             .get(`mushrooms.json`)
+//             .then((response) => ({
+//                 edible:mushrooms.json.edible,
+//                 description:mushrooms.json.description,
+
+.factory('mushroomFactory', function($http) {
+    return {
+        getMushroom : () => {
+           return $http.get('mushrooms.json')
+            .then((value) => {
+                console.log(value)
+                return value.data
+            })
+        }
     }
 })
--- app/factories/config.js
+++ app/factories/config.js
@@ -1,20 +1,20 @@
-app.config(($routeProvider, $locationProvider) => {
-    $locationProvider.hashPrefix('');
+// app.config(($routeProvider, $locationProvider) => {
+//     $locationProvider.hashPrefix('');
     
-    $routeProvider
-        .when('/', {
-            controller: 'MainCtrl',
-            templateUrl: 'partials/main.html'
-        })
-        .when('/list', {
-            controller: 'ListCtrl',
-            templateUrl: 'partials/list.html'
-        })
-        .when('/list/:listVar', {
-            controller:'DetailCtrl',
-            templateUrl:'partials/detail.html'
-            })
-        .otherwise({
-                redirectTo: '/'
-        })
-})
+//     $routeProvider
+//         .when('/', {
+//             controller: 'MainCtrl',
+//             templateUrl: 'partials/main.html'
+//         })
+//         .when('/list', {
+//             controller: 'ListCtrl',
+//             templateUrl: 'partials/list.html'
+//         })
+//         .when('/list/:listVar', {
+//             controller:'DetailCtrl',
+//             templateUrl:'partials/detail.html'
+//             })
+//         .otherwise({
+//                 redirectTo: '/'
+//         })
+// })
