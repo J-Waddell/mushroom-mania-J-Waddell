8ff7be57231fcc1a21a22dd38a6ad1eaf3786693
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-01-20 11:42:09 -0600

    displayed everything on main page, need to add search name to bring to seperate page for details etc..

--- app/app.js
+++ app/app.js
@@ -13,26 +13,13 @@ angular.module('MushroomMania', ['ngRoute'])
         })
     })
 
-.controller('RootCtrl', function($scope, $location) {
+.controller('RootCtrl', function($scope, $location, $http) {
     console.log('I am a RootCtrl')
     $scope.gotoMushroom = () => {
 
         $scope.gotoMushroom = () => $location.url(`/mushroom/${$scope.mushrooms}`)
     }
 })
-
-.controller('MushroomCtrl', function($scope, $routeParams, mushroomFactory) {
-    console.log('I am a MushroomCtrl')
-
-    mushroomFactory.getMushroom($routeParams.mushrooms)
-        .then((mushrooms) => {
-            $scope.name = mushrooms
-            $scope.edible = mushrooms.edible
-            $scope.description = mushrooms.description
-            // $scope.mushroom = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-        })
-    })
-
 .factory('mushroomFactory', ($http) => {
     return {
         getMushroom (mushroom) {
@@ -50,3 +37,15 @@ angular.module('MushroomMania', ['ngRoute'])
         },
     }
 })
+
+.controller('RootCtrl', function($scope, $routeParams, mushroomFactory) {
+    console.log('I am a MushroomCtrl')
+
+    mushroomFactory.getMushroom($routeParams.mushrooms)
+        .then((mushrooms) => {
+            $scope.name = mushrooms
+            // $scope.key = mushrooms.edible
+            // $scope.value = mushrooms.description
+            // $scope.mushroom = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        })
+    })
